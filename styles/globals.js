1eeba2a807ecab70ebe18661389d855c1397c7ca
makej7403-bit <makej7403@gmail.com>
2025-11-25 18:59:12 +0000

    Create globals.js

---
+++ styles/globals.js
@@ -0,0 +1,5 @@
+html, body, #__next { height: 100%; }
+body { margin: 0; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Arial; color: #111; padding-bottom: 60px; }
+a { color:#0070f3; text-decoration:none; }
+input, textarea, button { padding:8px; border:1px solid #ddd; border-radius:6px; }
+button { cursor:pointer; }
