4db41c82e690a5a11c7a5c469c2c675cec299099
makej7403-bit <makej7403@gmail.com>
2025-11-25 18:51:16 +0000

    Create library.js

---
+++ pages/library.js
@@ -0,0 +1,11 @@
+// pages/library.js
+import FileList from "../components/FileList";
+
+export default function LibraryPage() {
+  return (
+    <div>
+      <h1>Community Library</h1>
+      <FileList />
+    </div>
+  );
+}
