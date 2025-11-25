dd624f9745d4c13f0e797700fbfec3a55c6f0906
makej7403-bit <makej7403@gmail.com>
2025-11-25 19:01:37 +0000

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,24 @@
+// firebase/config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyC5hFB3ICxzyMrlvtnQl-n-2Dkr2RFsmqc",
+  authDomain: "fir-9b1f8.firebaseapp.com",
+  projectId: "fir-9b1f8",
+  storageBucket: "fir-9b1f8.firebasestorage.app",
+  messagingSenderId: "539772525700",
+  appId: "1:539772525700:web:25b5a686877ddbf6d176d1",
+  measurementId: "G-7FWY3QB5MY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
