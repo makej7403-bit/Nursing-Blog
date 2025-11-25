d6bddcc78fc5305ad3a3e3f408d03764dbedad6d
makej7403-bit <makej7403@gmail.com>
2025-11-25 21:37:05 +0000

    Update page.js

--- app/library/page.js
+++ app/library/page.js
@@ -1,34 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
-import { db } from "@/firebase/config";
-import { collection, onSnapshot } from "firebase/firestore";
 
-export default function Library() {
-  const [files, setFiles] = useState([]);
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, "library"), (snap) => {
-      setFiles(snap.docs.map((d) => ({ id: d.id, ...d.data() })));
-    });
-    return unsub;
-  }, []);
+import FileList from "@/components/FileList";
 
-  return (
-    <div className="p-6">
-      <h2 className="text-xl font-bold">Public Library</h2>
-
-      <div className="mt-4 space-y-3">
-        {files.map((f) => (
-          <a
-            key={f.id}
-            href={f.url}
-            target="_blank"
-            className="block p-3 border rounded"
-          >
-            {f.name}
-          </a>
-        ))}
-      </div>
-    </div>
-  );
+export default function LibraryPage() {
+  return <FileList />;
 }
